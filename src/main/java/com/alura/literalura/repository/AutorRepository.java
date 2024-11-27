a6154587f4bd7ca5a8ac7eaa3fd45b8936d74996
Boris <borisnandez93@gmail.com>
2024-11-26 22:13:15 -0600

    repository conexion a db

---
+++ src/main/java/com/alura/literalura/repository/AutorRepository.java
@@ -0,0 +1,13 @@
+package com.alura.literalura.repository;
+
+import com.alura.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("SELECT a FROM Autor a WHERE a.birth_day >= :year ORDER BY a.birth_day ASC")
+    List<Autor> findAuthorByDate(Integer year);
+}
